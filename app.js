cb797c10e3af8f8fc0aceb4da2244932987dca7d
Luciano Camargos <luciano.alvesdecamargos@gmail.com>
2022-10-01 11:28:30 -0300

    Initial commit do projeto

---
+++ app.js
@@ -0,0 +1,6 @@
+/* const express = require('express');
+const app = express();
+
+app.get('/', (resquest, response) => response.send('Funcionando'));
+
+app.listen(3030, () => console.log('Servidor funcionando na porta 3030!')) */
